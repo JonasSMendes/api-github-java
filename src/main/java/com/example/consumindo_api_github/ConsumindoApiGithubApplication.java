bbbbe9edef0b51f580e53f55b7c78724d5e24c67
jonas mendes <jonas.jsm2903@gmail.com>
2025-04-25 12:58:46 -0300

    create project

---
+++ src/main/java/com/example/consumindo_api_github/ConsumindoApiGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumindo_api_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumindoApiGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumindoApiGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consumindo_api_github/ConsumindoApiGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumindo_api_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumindoApiGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
