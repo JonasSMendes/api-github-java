ec8586c862bd9a8733696ccec442ca5369a6b390
jonas mendes <jonas.jsm2903@gmail.com>
2025-04-25 14:22:51 -0300

    implementation: repository response

--- src/main/java/com/example/consumindo_api_github/dto/RepositoryResponse.java
+++ src/main/java/com/example/consumindo_api_github/dto/RepositoryResponse.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record RepositoryResponse(
         String id,
         @JsonProperty("html_url") String htmlUrl,
+        String name,
         @JsonProperty("private") boolean isprivate
 
         ) {
